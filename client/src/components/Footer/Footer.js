1e524e2deeaa7ec9d460f1a21f0cb1f4186026e4
EAK <EAK@EAKXXXX.local>
2019-08-04 20:17:08 +0200

    make it better

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,8 +5,7 @@ function Footer() {
     return (
         <footer className="footer">
             <p><i className="fab fa-react react-icon"></i></p>
-            <p>Evan E J Tucker</p>
-            <p><a id="githib" href="https://github.com/evanejtucker/react-auth-simple" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
+            <p>James Njuguna</p>
         </footer>
     )
 }
